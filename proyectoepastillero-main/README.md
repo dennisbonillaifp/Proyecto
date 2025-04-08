72eda13880cb554f6cfcf16e1de340b162492be5
dennisbonillaifp <163356572+dennisbonillaifp@users.noreply.github.com>
2025-04-08 18:38:51 +0200

    Delete proyectoepastillero-main directory

--- proyectoepastillero-main/README.md
+++
@@ -1,2 +0,0 @@
-# proyectoepastillero
-# proyectoepastillero
--- proyectoepastillero-main/patrones/pruebaAdaptador.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("GestorCorreo.php");
-
-$enviarCorreo = new GestorCorreo('username');
-$enviarCorreo->sendMessage('Problema', 'No funciona algo', '6755642231');
-
-?>
